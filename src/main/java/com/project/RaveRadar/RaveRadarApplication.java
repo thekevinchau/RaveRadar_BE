4b3b40124440e58cd24533939a28eb9e7550a295
KEVIN CHAU <kevinch@udel.edu>
2025-06-09 15:32:32 -0400

    Initial commit

---
+++ src/main/java/com/project/RaveRadar/RaveRadarApplication.java
@@ -0,0 +1,13 @@
+package com.project.RaveRadar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaveRadarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaveRadarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/RaveRadar/RaveRadarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.RaveRadar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaveRadarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
